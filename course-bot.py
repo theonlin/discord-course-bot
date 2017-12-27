52e615af996c1707435a6176f975e65b8ccdd012
Theon Lin <theon_lin@petple.co>
2017-12-27 23:00:09 +0800

    Add help/version command

--- course-bot.py
+++ course-bot.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 import os
 import configparser
 
 import discord
 import asyncio
+
+__version__ = '0.01.01'
 
 # Parsing settings.cfg
 config = configparser.ConfigParser()
@@ -15,10 +18,41 @@ discord_token = config.get('Discord', 'Token') or ''
 bot_nickname = config.get('Bot', 'Nickname') or "Discord Course Bot"
 bot_status = config.get('Bot', 'Status') or "Writting Game Log"
 
-trigger = config.get('Bot', 'trigger') or '/cast'
+trigger = config.get('Bot', 'Trigger') or '/cast '
 
 # Init Discord Client
 client = discord.Client()
+
+
+def show_help(message, keyword, args):
+    ''' Show all commands or help descrition of single command
+    '''
+    global cmd_table
+    if len(args) > 0:
+        if args[0] in cmd_table.keys():
+            return client.send_message(message.channel, "%s:\t%s" % (args[0], cmd_table[args[0]]['desc']))
+    msg = []
+    for cmd in cmd_table.keys():
+        msg.append("%s:\t%s" % (cmd, cmd_table[cmd]['desc']))
+    return client.send_message(message.channel, "\n".join(msg))
+
+
+def show_version(message, keyword, args):
+    ''' Show version number of Discord Course Bot
+    '''
+    return client.send_message(message.channel, u"%s 目前的版本為(%s)" % (bot_nickname, __version__))
+
+
+cmd_table = {
+    'help': {
+        'desc': u'顯示所有可以使用的指令。',
+        'func': show_help
+    },
+    'version': {
+        'desc': u'顯示目前的版本。',
+        'func': show_version
+    }
+}
 
 
 @client.event
@@ -39,10 +73,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global trigger
+    global trigger, cmd_table
 
     if message.content.startswith(trigger):
-        await client.send_message(message.channel, '收到使用者指令： %s' % message.content[len(trigger):])
+        tokens = message.content[len(trigger):].split()
+        command = tokens[0] or ''
+        if command in cmd_table.keys():
+            cmd_obj = cmd_table[command]
+            await cmd_obj['func'](message, command, tokens[1:])
+        else:
+            await client.send_message(message.channel, u'用疑惑的眼神看著你...？ 請用 %s help 查詢指令' % trigger)
 
 
 client.run(discord_token)
